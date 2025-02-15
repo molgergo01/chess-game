00e1d78a0954bcb5b3e7906d356b579a49230b2c
molgergo01 <mol.gergo01@gmail.com>
2025-02-16 00:53:48 +0100

    fix login page grid

--- apps/frontend/src/components/ui/login/login-form.tsx
+++ apps/frontend/src/components/ui/login/login-form.tsx
@@ -10,9 +10,9 @@ export function LoginForm({
     return (
         <div className={cn('flex flex-col gap-6', className)} {...props}>
             <Card className="overflow-hidden">
-                <CardContent className="grid grid-rows-2 p-0 md:grid-cols-2">
+                <CardContent className="grid p-0 md:grid-cols-2 items-center">
                     <div className="p-6 md:p-8">
-                        <div className="flex flex-col gap-6">
+                        <div className="flex flex-col gap-6 md:gap-20">
                             <div className="flex flex-col items-center text-center">
                                 <h1 className="text-2xl font-bold">
                                     Welcome to Chess Game
@@ -21,9 +21,12 @@ export function LoginForm({
                                     Login to your Chess Game account
                                 </p>
                             </div>
+                            <div className="flex flex-col">
+                                <LoginButton />
+                            </div>
                         </div>
                     </div>
-                    <div className="relative hidden bg-muted md:block row-span-2 aspect-square">
+                    <div className="relative hidden bg-muted md:block aspect-square">
                         <Image
                             src="/board-login.png"
                             alt="An image of a chess board"
@@ -31,12 +34,6 @@ export function LoginForm({
                             height={200}
                             className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.4] dark:grayscale"
                         />
-                    </div>
-
-                    <div className="p-6 md:p-8 flex flex-col gap-6">
-                        <div className="flex flex-col">
-                            <LoginButton />
-                        </div>
                     </div>
                 </CardContent>
             </Card>
