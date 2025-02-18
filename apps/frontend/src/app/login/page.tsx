a7d3e16e38e67bbd2b191ce68f018488529ab5a4
molgergo01 <mol.gergo01@gmail.com>
2025-02-18 02:10:46 +0100

    Add suspense to make vercel work

--- apps/frontend/src/app/login/page.tsx
+++ apps/frontend/src/app/login/page.tsx
@@ -3,19 +3,22 @@
 import LoginForm from '@/components/login/login-form';
 import FailedLoginAlert from '@/components/login/failed-login-alert';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function LoginPage() {
     const searchParams = useSearchParams();
     const failedLoginParam = searchParams.has('failedLogin');
 
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-            <div className="w-full max-w-sm md:max-w-3xl p-6 md:p-8">
-                {failedLoginParam && <FailedLoginAlert />}
+        <Suspense>
+            <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+                <div className="w-full max-w-sm md:max-w-3xl p-6 md:p-8">
+                    {failedLoginParam && <FailedLoginAlert />}
+                </div>
+                <div className="w-full max-w-sm md:max-w-3xl">
+                    <LoginForm />
+                </div>
             </div>
-            <div className="w-full max-w-sm md:max-w-3xl">
-                <LoginForm />
-            </div>
-        </div>
+        </Suspense>
     );
 }
