6e76edb6369c658fc932dcae4676dcb9671f0ff9
molgergo01 <mol.gergo01@gmail.com>
2025-02-18 02:14:06 +0100

    Fix suspense

--- apps/frontend/src/app/login/page.tsx
+++ apps/frontend/src/app/login/page.tsx
@@ -6,19 +6,25 @@ import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 
 export default function LoginPage() {
+    return (
+        <Suspense>
+            <PageBody />
+        </Suspense>
+    );
+}
+
+function PageBody() {
     const searchParams = useSearchParams();
     const failedLoginParam = searchParams.has('failedLogin');
 
     return (
-        <Suspense>
-            <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-                <div className="w-full max-w-sm md:max-w-3xl p-6 md:p-8">
-                    {failedLoginParam && <FailedLoginAlert />}
-                </div>
-                <div className="w-full max-w-sm md:max-w-3xl">
-                    <LoginForm />
-                </div>
+        <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+            <div className="w-full max-w-sm md:max-w-3xl p-6 md:p-8">
+                {failedLoginParam && <FailedLoginAlert />}
             </div>
-        </Suspense>
+            <div className="w-full max-w-sm md:max-w-3xl">
+                <LoginForm />
+            </div>
+        </div>
     );
 }
