48a8c11c7a5d13706317571eef0dc06c10fe66ab
molgergo01 <mol.gergo01@gmail.com>
2025-02-24 00:39:49 +0100

    Remove cookie in frontend if fetch fails

--- apps/frontend/src/components/layout/logout-button-dropdown.tsx
+++ apps/frontend/src/components/layout/logout-button-dropdown.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { DropdownMenuItem } from '@radix-ui/react-dropdown-menu';
+import { cookies } from 'next/headers';
 
 export default function LogoutButton() {
     return (
@@ -10,10 +11,16 @@ export default function LogoutButton() {
     );
 
     async function handleLogout() {
-        await fetch('http://localhost:8080/api/auth/logout', {
-            method: 'POST',
-            credentials: 'include'
-        });
+        try {
+            await fetch('http://localhost:8080/api/auth/logout', {
+                method: 'POST',
+                credentials: 'include'
+            });
+        } catch (e) {
+            const cookieStore = await cookies();
+            cookieStore.delete('token');
+            console.log(e);
+        }
         window.location.href = 'http://localhost:3000/login';
     }
 }
