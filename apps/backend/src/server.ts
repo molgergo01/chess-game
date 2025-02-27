f56af6694b318617a4dcca86619fd448710b3389
molgergo01 <mol.gergo01@gmail.com>
2025-02-27 01:22:19 +0100

    Add socket.io server

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,8 +1,24 @@
 import app from './app';
 import env from './config/env';
+import { createServer } from 'node:http';
+import { Server, Socket } from 'socket.io';
 
 const PORT = env.PORT || 8080;
+const server = createServer(app);
 
-app.listen(env.PORT, () => {
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
+
+const onConnection = (socket: Socket) => {
+    console.log(`New connection on socket.io`);
+    socket.on('message', (message) => {
+        socket.emit('message', `I've received: ${message}`);
+    });
+};
+
+io.on('connection', onConnection);
+
+server.listen(env.PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
